365bd7211429fc971cf37035ddddc9f69b04d000
gamoradima <gamoradima@gmail.com>
2024-12-12 14:50:43 +0200

    Realty app added

--- UsrDevPackageUA/SqlScripts/ContactAgeDaysMSSQL/ContactAgeDaysMSSQL.sql
+++ UsrDevPackageUA/SqlScripts/ContactAgeDaysMSSQL/ContactAgeDaysMSSQL.sql
@@ -1,4 +1,5 @@
 create or alter view UsrVwContactAgeDays as
 select Id as UsrId, Name as UsrName, BirthDate as UsrBirthDate,
-  datediff(day, BirthDate, getdate()) as UsrAgeDays
+  datediff(day, BirthDate, getdate()) as UsrAgeDays,
+  Id as UsrContactId
 from Contact
